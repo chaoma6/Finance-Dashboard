b2f2a39a5a5e257ff30b687a5a1a0b75da42a89c
Chao Ma <machaosydney@gmail.com>
2023-07-24 10:53:22 +1000

    Style: create theme.ts

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,30 @@
-import React from "react"
+import { createTheme } from '@mui/material/styles';
+import { useMemo } from 'react';
+import { themeSettings } from './theme';
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Box } from '@mui/material';
 
-function App(){
-
-      return <div className='app'></div>
-  
+function App() {
+  const theme = useMemo(() => createTheme(themeSettings), []);
+  return (
+    <div className="app">
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Box width="100%" height="100%" padding="1rem 2rem 4rem 2rem">
+            <Routes>
+              <Route path="/" element={<div>dashboard page</div>}></Route>
+              <Route
+                path="/predictions"
+                element={<div>predictions page</div>}
+              ></Route>
+            </Routes>
+          </Box>
+        </ThemeProvider>
+      </BrowserRouter>
+    </div>
+  );
 }
 
-export default App
+export default App;
