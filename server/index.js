ab693cfe77ec7c479bf73bd6a04874e3b9996cd2
Chao Ma <machaosydney@gmail.com>
2023-07-26 17:21:52 +1000

    feat: Implement KPI routes and server config

---
+++ server/index.js
@@ -0,0 +1,42 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import kpiRoutes from './routes/kpi.js';
+import { kpis } from './data/data.js';
+import KPI from './models/KPI.js';
+
+/* CONFIGURATIONS */
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: 'cross-origin' }));
+app.use(morgan('common'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+
+/* ROUTES */
+app.use('/kpi', kpiRoutes);
+
+/* MongoDB*/
+const PORT = process.env.PORT || 9000;
+mongoose
+	.connect(process.env.MONGO_URL, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	})
+	.then(async () => {
+		app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
+
+		/* ADD DATA ONE TIME ONLY OR AS NEEDED */
+		// await mongoose.connection.db.dropDatabase();
+		// KPI.insertMany(kpis);
+		// Product.insertMany(products);
+		// Transaction.insertMany(transactions);
+	})
+	.catch((error) => console.log(`${error} did not connect`));
