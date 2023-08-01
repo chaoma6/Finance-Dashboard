36f1d2d4f1542029c72913538be63a3df51b2f33
Chao Ma <machaosydney@gmail.com>
2023-08-01 23:44:24 +1000

    feat: add dashboard rows

--- client/src/scenes/dashboard/Row3.tsx
+++ client/src/scenes/dashboard/Row3.tsx
@@ -9,6 +9,7 @@ const Row3 = (props: Props) => {
       <DashboardBox gridArea="g"></DashboardBox>
       <DashboardBox gridArea="h"></DashboardBox>
       <DashboardBox gridArea="i"></DashboardBox>
+      <DashboardBox gridArea="j"></DashboardBox>
     </>
   );
 };
